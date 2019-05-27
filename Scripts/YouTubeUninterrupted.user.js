4e65685694a4958529dc17d08221d32f5b2ad32b
Zach <zjc2920@g.rit.edu>
2019-05-27 16:32:07 -0400

    How did those tabs get there...

--- Scripts/YouTubeUninterrupted.user.js
+++ Scripts/YouTubeUninterrupted.user.js
@@ -14,18 +14,18 @@
     setInterval(function() {
         var btn = document.querySelectorAll('ytd-popup-container paper-dialog.ytd-popup-container          \
                                              yt-confirm-dialog-renderer.ytd-popup-container                \
-																					   div#main.yt-confirm-dialog-renderer                           \
+                                             div#main.yt-confirm-dialog-renderer                           \
                                              div.buttons.yt-confirm-dialog-renderer                        \
-																						 yt-button-renderer#confirm-button.yt-confirm-dialog-renderer  \
-																					   a.yt-simple-endpoint.yt-button-renderer                       \
-																					   paper-button#button.yt-button-renderer')[0];
+                                             yt-button-renderer#confirm-button.yt-confirm-dialog-renderer  \
+                                             a.yt-simple-endpoint.yt-button-renderer                       \
+                                             paper-button#button.yt-button-renderer')[0];
       	// Check if button exists
         if (btn !== undefined) {
             btn.click();
-          	// Remove popup window
-          	document.querySelectorAll('ytd-popup-container')[0].remove();
-          	// Check if video paused, resume if so
-          	document.querySelectorAll("ytd-app div#movie_player.paused-mode")[0].click()
+            // Remove popup window
+            document.querySelectorAll('ytd-popup-container')[0].remove();
+            // Check if video paused, resume if so
+            document.querySelectorAll("ytd-app div#movie_player.paused-mode")[0].click()
         }
     }, 500);  // every .5 seconds
 })();
